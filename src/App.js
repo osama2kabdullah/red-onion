ee3ea172c8840cf7b95a1910b397318a2551e0c1
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-17 16:41:07 +0600

    footer complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Pages/shared/Header';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/shared/Footer';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
